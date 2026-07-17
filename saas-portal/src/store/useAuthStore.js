39ac8bb271da2850a6e91947c088dbb3c83b6713
allygitrepo <allygithub@gmail.com>
2026-07-17 17:17:52 +0530

    Refresh token configuration

--- saas-portal/src/store/useAuthStore.js
+++ saas-portal/src/store/useAuthStore.js
@@ -6,11 +6,17 @@ const useAuthStore = create()(
     (set) => ({
       user: null,
       token: null,
+      refreshToken: null,
       isAuthenticated: false,
       
-      setAuth: (user, token) => set({ user, token, isAuthenticated: !!token }),
+      setAuth: (user, token, refreshToken) => set((state) => ({ 
+        user, 
+        token, 
+        refreshToken: refreshToken !== undefined ? refreshToken : state.refreshToken, 
+        isAuthenticated: !!token 
+      })),
       
-      logout: () => set({ user: null, token: null, isAuthenticated: false }),
+      logout: () => set({ user: null, token: null, refreshToken: null, isAuthenticated: false }),
       
       updateUser: (userData) => set((state) => ({ 
         user: { ...state.user, ...userData } 
