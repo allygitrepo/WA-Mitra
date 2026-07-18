15804b07a529812ef93cd963ba5edcd38d8e19b7
aniketsol456 <aniket.solanki.9054@gmail.com>
2026-07-18 18:11:16 +0530

    fixed the bug of db connection.

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,13 +2,13 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DB_NAME || "postgres",
+    process.env.DB_USER || "postgres",
+    process.env.DB_PASS || "postgres",
     {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        port: process.env.DB_PORT,
+        host: process.env.DB_HOST || "localhost",
+        dialect: process.env.DB_DIALECT || "postgres",
+        port: process.env.DB_PORT || 5432,
         logging: false, // disable SQL logs
         pool: {
             max: 25,
